51dbbced6d90a4d76c68843d3f3c2544d48b7ee9
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-06-26 17:53:53 -0500

    feat: :sparkles: Obtener data de rockets
    se agrego una funcion en el main.js con el fin de testear el funcionamiento de los query e ir retornando la data de el enpoind de rockets y starlink

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 
+const test = async() =>{
+    
+    let config ={
+        method:'POST',
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify({
+            query:{},
+            options:{select:['launch']}
+        })
+    }
+    // let res = await fetch(`https://api.spacexdata.com/v4/rockets/query`,config);
+    let res = await fetch(`https://api.spacexdata.com/v4/starlink/query`,config)
+    let data = res.json();
+    return data;
+};
+
+let data = await test()
+console.log(data)
